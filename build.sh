9da725c062dfae89b0da5c1bed93bda6c6d51039
Your Name <44114870+JdavisBro@users.noreply.github.com>
2025-10-22 14:40:55 +0100

    rename project

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 "/c/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/MSBuild/Current/Bin/MSBuild.exe" /property:Configuration=Release
-cp "x64/Release/InfoWindow.dll" "$beastieball/mods/aurie/"
+cp "x64/Release/BeastieballInfoWindow.dll" "$beastieball/mods/aurie/"
