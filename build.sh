7d1918f3778b862539b174c8022ddd8038a6cfcd
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2026-02-04 15:51:10 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 "/c/Program Files/Microsoft Visual Studio/18/Community/MSBuild/Current/Bin/MSBuild.exe" /property:Configuration=Release
-cp "x64/Release/BeastieballInfoWindow.dll" "$beastieball/mods/aurie/"
+cp "x64/Release/BeastieballInfoWindow-yytk_v4.dll" "$beastieball/mods/aurie/"
