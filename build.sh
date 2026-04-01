aedb9ecc3a64a5ee134623d5a63053980ece7b7f
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2026-04-01 03:05:48 +0100

    rename dll

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 "/c/Program Files/Microsoft Visual Studio/18/Community/MSBuild/Current/Bin/MSBuild.exe" /property:Configuration=Release
-cp "x64/Release/BeastieballInfoWindow.dll" "$beastieball/mods/aurie/"
+cp "x64/Release/BeastieballGacha.dll" "$beastieball/mods/aurie/"
